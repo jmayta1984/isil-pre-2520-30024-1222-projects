4b86b9ca0ecbcca8f2f075e90ddc0f38eae961a1
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-17 14:51:11 -0500

    feat (home): update home screen

--- EasyShop/app/src/main/java/pe/isil/easyshop/Home.kt
+++ EasyShop/app/src/main/java/pe/isil/easyshop/Home.kt
@@ -1,11 +1,16 @@
 package pe.isil.easyshop
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -15,6 +20,9 @@ import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -22,7 +30,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import pe.isil.easyshop.ui.theme.EasyShopTheme
@@ -80,9 +93,88 @@ fun Home() {
             )
             RoundedIcon(Icons.Default.ArrowDropDown)
         }
+
+        BannerCard()
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text("Categories")
+            Text("See all")
+        }
+
+        Row {
+            Column {
+                Box(modifier = Modifier
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.surfaceVariant)) {
+                    Image(
+                        painterResource(R.drawable.banner),
+                        contentDescription = null,
+                        modifier = Modifier.size(96.dp)
+                    )
+
+                }
+                Text("Women")
+            }
+
+        }
+
     }
 }
 
+@Composable
+fun BannerCard() {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .height(192.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(
+                brush = Brush.linearGradient(
+                    listOf(
+                        MaterialTheme.colorScheme.primary,
+                        MaterialTheme.colorScheme.surfaceVariant
+                    )
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "Get your special sale up 40%",
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.background,
+                fontWeight = FontWeight.SemiBold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    contentColor = MaterialTheme.colorScheme.primary
+                )
+            ) {
+                Text("Shop now")
+            }
+        }
+        Image(
+            painterResource(R.drawable.banner),
+            contentDescription = null,
+            modifier = Modifier.weight(1f),
+            contentScale = ContentScale.FillWidth
+        )
+    }
+}
 
 @Composable
 fun RoundedIcon(icon: ImageVector) {
