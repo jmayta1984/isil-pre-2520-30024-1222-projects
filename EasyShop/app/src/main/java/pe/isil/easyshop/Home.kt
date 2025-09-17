1483d67bdbb1a3333beb38a719688307651aacf7
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-17 12:51:04 -0500

    feat (home): add home screen

---
+++ EasyShop/app/src/main/java/pe/isil/easyshop/Home.kt
@@ -0,0 +1,117 @@
+package pe.isil.easyshop
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pe.isil.easyshop.ui.theme.EasyShopTheme
+
+@Composable
+fun Home() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(
+                        MaterialTheme
+                            .colorScheme.primary
+                    )
+            ) {
+                Icon(
+                    Icons.Default.Person,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+            Column(modifier = Modifier.weight(1f)) {
+                Text("Hello Alex")
+                Text("Good morning")
+            }
+
+            RoundedIcon(Icons.Outlined.Notifications)
+
+            RoundedIcon(Icons.Outlined.ShoppingCart)
+
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            OutlinedTextField(
+                "", onValueChange = {},
+                modifier = Modifier.weight(1f),
+                placeholder = {
+                    Text("Search")
+                }
+            )
+            RoundedIcon(Icons.Default.ArrowDropDown)
+        }
+    }
+}
+
+
+@Composable
+fun RoundedIcon(icon: ImageVector) {
+    Box(
+        modifier = Modifier
+            .size(48.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(
+                MaterialTheme
+                    .colorScheme.surfaceVariant
+            )
+    ) {
+        Icon(
+            icon,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            tint = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun HomePreview() {
+    EasyShopTheme(dynamicColor = false) {
+        Home()
+    }
+
+}
