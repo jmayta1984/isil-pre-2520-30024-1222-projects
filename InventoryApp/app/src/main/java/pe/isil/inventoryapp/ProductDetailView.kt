0c3a37e90b9ae2a3775364053282c9eaa0478b63
jmayta1984 <jmayta@pucp.edu.pe>
2025-10-15 12:11:40 -0500

    chore: update project

--- InventoryApp/app/src/main/java/pe/isil/inventoryapp/ProductDetailView.kt
+++ InventoryApp/app/src/main/java/pe/isil/inventoryapp/ProductDetailView.kt
@@ -1,9 +1,85 @@
 package pe.isil.inventoryapp
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+@Preview(showBackground = true)
 @Composable
-fun ProductDetailView(modifier: Modifier = Modifier) {
+fun ProductDetailView(
+    modifier: Modifier = Modifier,
+    product: Product? = null
+) {
 
+    val name = remember { mutableStateOf("") }
+    val quantity = remember { mutableStateOf("") }
+    val price = remember { mutableStateOf("") }
+
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = {}) {
+            Icon(
+                Icons.Default.Done,
+                contentDescription = "add"
+            )
+        }
+    }) { paddingValues ->
+
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.Center
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                value = name.value, onValueChange = { value ->
+                    name.value = value
+                },
+                placeholder = {
+                    Text(text = "Name")
+                }
+            )
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                value = quantity.value, onValueChange = { value ->
+                    quantity.value = value
+                },
+                placeholder = {
+                    Text(text = "Quantity")
+                }
+            )
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                value = price.value, onValueChange = { value ->
+                    price.value = value
+                },
+                placeholder = {
+                    Text(text = "Price")
+                }
+            )
+        }
+    }
 }
